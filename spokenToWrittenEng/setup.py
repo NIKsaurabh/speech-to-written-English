fdf616709453c678b95a3c32704d8eac3359ea47
Saurabh Kumar <saurabh238238@gmail.com>
2021-05-30 00:05:22 +0530

    Add files via upload

---
+++ spokenToWrittenEng/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name='spokenToWrittenEng',
+    packages=find_packages(),
+    version='0.1.0',
+    description='convert speech to written english',
+    author='Saurabh',
+)

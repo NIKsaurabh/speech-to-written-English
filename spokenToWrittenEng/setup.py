067e22f3b1f0867ab445c28b7c33f10a9cd50fee
Saurabh Kumar <saurabh238238@gmail.com>
2021-05-30 08:33:31 +0530

    Delete spokenToWrittenEng directory

--- spokenToWrittenEng/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-setup(
-    name='spokenToWrittenEng',
-    packages=find_packages(),
-    version='0.1.0',
-    description='convert speech to written english',
-    author='Saurabh',
-)
